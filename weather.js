602c0ef7c553a01a3e00951024a40bf685f1052e
Ken-Yeung <66005509+Ken-Yeung@users.noreply.github.com>
2020-07-19 10:50:04 +0800

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,21 +1,16 @@
 window.addEventListener('load', ()=> {
-    let long;
-    let lat;
+    let temperatureDegree = document.querySelector('.temperature-degree')
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
+    const api = 'https://api.openweathermap.org/data/2.5/onecall?lat=22.29&lon=114.16&exclude=minutely,hourly&lang=zh_tw&appid=0ef99421ced3a49c3f775f6149a4dd8e&units=metric'
 
-            const api = 'https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely,hourly&lang=zh_tw&appid=0ef99421ced3a49c3f775f6149a4dd8e&units=metric'
-
-            fetch(api)
-                .then(response => {
-                    return response.json();
-                })
-                .then(data => {
-                    console.log(data);
-                });
+    fetch(api)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            const {temp} = data.current;
+            //set DOM Element from API
+            temperatureDegree.textContent = temp;
         });
-    }
 });
